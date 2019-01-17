41b27652f0abc8b655000c2212c445f535fdaf49
alex <Alex@AlexLiudeMBP.princeton.epam.com>
2019-01-17 14:04:06 +0800

    finish analyze-page-layout

--- screens/AnalyzeScreen.js
+++ screens/AnalyzeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { Icon } from "expo";
 
 export default class AnalyzeScreen extends React.Component {
   static navigationOptions = {
@@ -9,19 +9,75 @@ export default class AnalyzeScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View>
+        <View style={{backgroundColor: 'green', paddingTop: 60, flexDirection: 'column', alignItems: 'center'}}>
+          <Text style={{color: 'white', fontSize: 20}}>分析</Text>
+        </View>
+        <View style={{flexDirection: 'column', alignItems: 'center', height: 100, justifyContent: 'center'}}>
+          <Text style={{fontSize:15}}>当前藏书</Text>
+          <Text style={{fontSize: 25}}>1</Text>
+        </View>
+
+        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>
+          <View style={styles.singleCountContainer}>
+            <Icon.EvilIcons name="pencil" size={20}/>
+            <Text>最爱作者</Text>
+          </View>
+          <View style={styles.singleCountContainer}>
+            <Icon.EvilIcons name="navicon" size={20}/>
+            <Text>出版社</Text>
+          </View>
+          <View style={styles.singleCountContainer}>
+            <Icon.EvilIcons name="tag" size={20}/>
+            <Text>常用标签</Text>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>
+          <View style={styles.singleCountContainer}>
+            <Icon.EvilIcons name="chart" size={20}/>
+            <Text>书柜管理</Text>
+          </View>
+          <View style={styles.singleCountContainer}>
+            <Icon.MaterialCommunityIcons name="account-outline" size={20}/>
+            <Text>借出管理</Text>
+          </View>
+          <View style={styles.singleCountContainer}>
+            <Icon.MaterialCommunityIcons name="coffee-outline" size={20}/>
+            <Text>阅读进度</Text>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>
+          <View style={styles.singleCountContainer}>
+            <Icon.AntDesign name="shoppingcart" size={20}/>
+            <Text>购买渠道</Text>
+          </View>
+          <View style={styles.invisible}>
+            <Icon.AntDesign name="shoppingcart" size={20}/>
+            <Text>购买渠道</Text>
+          </View>
+          <View style={styles.invisible}>
+            <Icon.AntDesign name="shoppingcart" size={20}/>
+            <Text>购买渠道</Text>
+          </View>
+        </View>
+
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    
   },
+  singleCountContainer: {
+    flexDirection:'column',
+    alignItems:'center',
+    height: 80,
+  },
+  invisible: {
+    opacity: 0
+  }
 });
