6318e8b4827e48ff196dddfd5f44a622acc1d78d
alex <Alex@AlexLiudeMBP.princeton.epam.com>
2019-01-21 13:50:00 +0800

    finish asyncstorage

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import DeviceStorage from './utils/DeviceStorage';
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+
+  componentDidMount() {
+    // DeviceStorage.delete('books')
+  }
 
   render() {
 
